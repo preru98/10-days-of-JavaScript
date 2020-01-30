51151dd02f936b2a42c76f7532f38b5a41af10cb
Prerna <prerna1998.ps@gmail.com>
2020-01-30 23:53:28 +0530

    Day 1 challenge part 3

---
+++ day-1-part3.js
@@ -0,0 +1,32 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    const PI=Math.PI;
+    let r=readLine();
+    console.log(PI*r*r);
+    console.log(2*PI*r);
+
+    try {    
