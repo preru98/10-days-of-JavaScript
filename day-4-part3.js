af50d0c6b1432deed3476b84b541495b9f148842
Prerna <prerna1998.ps@gmail.com>
2020-02-02 23:18:23 +0530

    Day 4 Challenge 3

---
+++ day-4-part3.js
@@ -0,0 +1,17 @@
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+class Polygon{
+    constructor(sides){
+        this.sides=sides;
+    }
+    perimeter(){
+        let sum=0;
+        for(let i=0;i<this.sides.length;i++){
+            sum+=this.sides[i];
+        }
+        return sum;
+    }
+}
